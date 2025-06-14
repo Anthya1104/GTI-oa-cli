ecb57c3d82da09f946b418a5ec6931406e39c64e
Anthya1104 <anthya1104@gmail.com>
2025-06-14 18:48:46 +0800

    feat:adjusted cli init settings and debug tool settings

--- math-game/internal/config/config.go
+++ math-game/internal/config/config.go
@@ -1,10 +1,12 @@
 package config
 
+var Version = "dev"
+
 const (
 	LogLevelDebug   string = "debug"
 	LogLevelInfo    string = "info"
 	LogLevelWarning string = "warn"
 	LogLevelError   string = "error"
 
-	LogFilePath string = "math-game/log/log_output.txt"
+	LogFilePath string = "log/log_output.txt"
 )

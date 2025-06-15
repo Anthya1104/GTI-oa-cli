b207d2faf7af2fb000bdf6cde1dd1e99466bed4f
Anthya1104 <anthya1104@gmail.com>
2025-06-15 17:30:23 +0800

    feat/test:added game play mechanism and simple flow positive test case

--- math-game/internal/model/game_test.go
+++ math-game/internal/model/game_test.go
@@ -1,27 +1,14 @@
-package main
+package model_test
 
 import (
 	"math/rand"
-	"os"
+	"testing"
 	"time"
 
-	"github.com/Anthya1104/math-game-cli/internal/cobra"
-	"github.com/Anthya1104/math-game-cli/internal/config"
-	"github.com/Anthya1104/math-game-cli/internal/logger"
 	"github.com/Anthya1104/math-game-cli/internal/model"
-	"github.com/sirupsen/logrus"
 )
 
-func main() {
-
-	if err := logger.InitLogger(config.LogLevelInfo); err != nil {
-		logrus.Fatalf(("Error initializing Logger : %v"), err)
-	}
-
-	if err := cobra.ExecuteCmd(); err != nil {
-		logrus.Fatalf("Error executing command: %v", err)
-		os.Exit(1)
-	}
+func TestGamePlay_SimpleFlow(t *testing.T) {
 
 	students := []*model.Student{
 		model.NewStudent("A", 1),
@@ -46,4 +33,7 @@ func main() {
 	}
 	game.Start()
 
+	if len(game.Questions) != 1 {
+		t.Errorf("expected 1 question, got %d", len(game.Questions))
+	}
 }

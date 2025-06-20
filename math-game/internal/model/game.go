7a885885cfc9640499368c4d1c5e1919cef999c8
Anthya1104 <anthya1104@gmail.com>
2025-06-20 20:05:25 +0800

    feat:added bonus1 implementation, added random answer generating and allow next student to answer if the answer is wrong

--- math-game/internal/model/game.go
+++ math-game/internal/model/game.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"math/rand"
 	"time"
 
 	"github.com/sirupsen/logrus"
@@ -40,28 +41,45 @@ func (g *Game) PlayRound(q *Question) {
 	logrus.Infof("Teacher: %s", q)
 
 	answerCh := make(chan AnswerEvent, len(g.Students))
-
 	for _, s := range g.Students {
 		go AskStudent(s, q, answerCh)
 	}
 
-	// wait for the first student to answer
-	first := <-answerCh
+	attemptedStudents := make(map[string]bool)
+	var winner *Student
 
-	logrus.Infof("%s answered %d", first.Student.Name, first.Answer)
+	// Loop to process answers until a winner is found or everyone has tried
+	for len(attemptedStudents) < len(g.Students) {
+		answer := <-answerCh
 
-	if first.Answer == q.Answer {
-		g.Results = append(g.Results, RoundResult{Student: first.Student, Answer: first.Answer})
-		logrus.Infof("Teacher: %s, you're right!", first.Student.Name)
-	} else {
-		logrus.Infof("Teacher: %s, wrong answer!", first.Student.Name)
+		// Skip if this student has already attempted in this round
+		if _, ok := attemptedStudents[answer.Student.Name]; ok {
+			continue
+		}
+
+		attemptedStudents[answer.Student.Name] = true
+		logrus.Infof("%s: %d %s %d = %d!", answer.Student.Name, q.ArgumentA, q.Operator, q.ArgumentB, answer.Answer)
+
+		// Check if the answer is correct
+		if answer.Answer == q.Answer {
+			logrus.Infof("Teacher: %s, you're right!", answer.Student.Name)
+			winner = answer.Student
+			g.Results = append(g.Results, RoundResult{Student: winner, Answer: answer.Answer})
+			break
+		} else {
+			logrus.Infof("Teacher: %s, you are wrong.", answer.Student.Name)
+		}
 	}
 
-	// interaction from other students
-	for _, s := range g.Students {
-		if s.Name != first.Student.Name {
-			logrus.Infof("%s: %s, you win", s.Name, first.Student.Name)
+	// After the loop, check if we found a winner
+	if winner != nil {
+		for _, s := range g.Students {
+			if s.Name != winner.Name {
+				logrus.Infof("%s: %s, you win.", s.Name, winner.Name)
+			}
 		}
+	} else {
+		logrus.Infof("Teacher: Boooo~ Answer is %d.", q.Answer)
 	}
 }
 
@@ -69,9 +87,22 @@ func (g *Game) PlayRound(q *Question) {
 var AskStudent = func(s *Student, q *Question, ch chan AnswerEvent) {
 	time.Sleep(s.WaitTime)
 
+	studentAnswer := q.Answer
+	//50% chance of getting the wrong answer
+	if rand.Intn(2) == 0 { // 0 represents a wrong answer
+		// Generate a random wrong answer that is not the correct one
+		for {
+			wrongAnswer := q.Answer + (rand.Intn(10) - 5) // a random number near the answer
+			if wrongAnswer != q.Answer {
+				studentAnswer = wrongAnswer
+				break
+			}
+		}
+	}
+
 	ch <- AnswerEvent{
 		Student: s,
-		Answer:  q.Answer, // would always as right answer in this state
+		Answer:  studentAnswer,
 		QID:     q.ID,
 		Time:    time.Now(),
 	}
